ec11acc21f4e810c79700501ad6c9ccb0d64ee9a
uptimax7 <30338166+uptimax7@users.noreply.github.com>
2020-02-16 02:56:59 +0000

    Update push.js

--- push.js
+++ push.js
@@ -6,4 +6,8 @@ endpoint = { publicKey:
     'BDPGfy5CLOxEPO-EVEbBpgT-WtSdWvBPJa4tt_LlZKtSAeeYUi5k0hpMHy7pFv3L5lYX4sh2T_fg61D9rK-OryM',
    privateKey: 'Zspc_H_67-R9sxfs3DVfwmbtiGL4q2r3sp9eJngglTA' };
 
-console.log(keys);
+push.setVapidDetails('uptimax7@gmail.com', endpoint.publicKey, endpoint.privateKey);
+
+sub = {};
+
+push.sendNotification(sub, 'this is awesome');
