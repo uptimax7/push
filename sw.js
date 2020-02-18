9690a79405222a06af1c7ffe4929ffe1ce328c8f
uptimax7 <30338166+uptimax7@users.noreply.github.com>
2020-02-18 01:21:08 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,25 @@
-self.addEventListener('push', evt=>{
- console.log(evt)
- }
- );
+var asset = ['close.png','message.png'];
+var cacheName ='site-staic-v1';
+self.addEventListener('install', evt=>{
+ evt.waitUntil(
+  caches.open(cacheName).then(cache=>{
+   cache.addAll(asset);
+  }
+    )
+  });
+ 
+ self.addEventListener('push', evt=>{
+var data = evt.data.text();
+
+options = {
+    body: 'this is awesome',
+    actions:[
+        {action:'explore',title:'let get awesome', icon:''},
+        {action:'close',title:'get awesome later', icon:''}
+    ]
+};
+
+evt.waitUntil(
+self.registration.showNotification(data, options)
+)
+});
